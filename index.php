068349e911e9e7bdbe3414c45230bf3a3991fcb8
MabiGermano <mabi.germano.e@gmail.com>
2018-12-09 19:45:16 -0300

    Eventos do formulário

--- index.php
+++ index.php
@@ -54,21 +54,21 @@
         <form>
         <div>
             <label for="nome" id="label-nome" class="label-form"></label>
-            <input type="text" name="nome" id="nome" class="linha1" placeholder="Nome" alt="Nome" required>
+            <input type="text" name="nome" id="nome" class="linha1" placeholder="Nome*" onkeydown="funcNome()" onblur="sumirNome()" alt="Nome" required>
         </div>
         
         <div id="div-assunto">
             <label for="assunto" id="label-assunto" class="label-form"></label>
-            <input type="text" name="assunto" id="assunto" class="linha2" placeholder="Assunto" alt="Assunto" required>
+            <input type="text" name="assunto" id="assunto" class="linha2" placeholder="Assunto*" onkeydown="funcAssunto()" onblur="sumirAssunto()" alt="Assunto" required>
         </div>
 
         <div id="div-email">
             <label for="email" id="label-email" class="label-form"></label>
-            <input type="email" name="email" id="email" onkeydown="funcEmail()" onblur="sumir()" class="linha2" placeholder="Email" alt="Email" required>
+            <input type="email" name="email" id="email" onkeydown="funcEmail()" onblur="sumirEmail()" class="linha2" placeholder="Email*" alt="Email" required>
         </div>
         <div id="div-mensagem">
             <label for="mensagem" id="label-mensagem" class="label-form"></label>
-            <textarea name="mensagem" id="mensagem" class="linha3" cols="30" rows="10" placeholder="Mensagem" alt="Mensagem" required></textarea>
+            <textarea name="mensagem" id="mensagem" class="linha3" cols="30" rows="10" placeholder="Mensagem*" onkeydown="funcMensagem()" onblur="sumirMensagem()" alt="Mensagem" required></textarea>
         </div>
         
         <button type="submit">Enviar</button>
